fedfc87048489c3660ce93555845a6130965706d
Lux14 <luxjuneau111@gmail.com>
2022-12-18 23:24:35 -0500

    Made some changes to the names, and tidy.

--- File Encryptor/MainFRM.cs
+++ File Encryptor/MainFRM.cs
@@ -9,68 +9,93 @@ namespace File_Encryptor
 {
     public partial class MainFRM : Form
     {
-        public MainFRM() => InitializeComponent();
+        public MainFRM() => 
+            InitializeComponent();
 
-        private void button1_Click(object sender, EventArgs e) => listBox1.Items.Remove(listBox1.SelectedItem);
+        private void button1_Click(object sender, EventArgs e) => 
+            listBox1.Items.Remove(listBox1.SelectedItem);
 
         private void button2_Click(object sender, EventArgs e)
         {
-            using (var openFileDialog = new OpenFileDialog())
+            try
             {
-                openFileDialog.Filter = "All Files (*.*)|*.*";
-                openFileDialog.Multiselect = true;
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                using (var openFileDialog = new OpenFileDialog())
                 {
-                    foreach (var file in openFileDialog.FileNames)
-                        listBox1.Items.Add(file);
+                    openFileDialog.Filter = "All Files (*.*)|*.*";
+                    openFileDialog.Multiselect = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        foreach (var file in openFileDialog.FileNames)
+                            listBox1.Items.Add(file);
+                    }
                 }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show($"{error.Message}\n\n{error.StackTrace}", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            using (var folderBrowserDialog = new FolderBrowserDialog())
+            try
             {
-                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
-                    listBox2.Items.Add(folderBrowserDialog.SelectedPath);
+                using (var folderBrowserDialog = new FolderBrowserDialog())
+                {
+                    if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                        listBox2.Items.Add(folderBrowserDialog.SelectedPath);
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show($"{error.Message}\n\n{error.StackTrace}", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        private void button4_Click(object sender, EventArgs e) => listBox2.Items.Remove(listBox2.SelectedItem);
+        private void button4_Click(object sender, EventArgs e) => 
+            listBox2.Items.Remove(listBox2.SelectedItem);
 
-        private void button5_Click(object sender, EventArgs e) => textBox1.Text = GeneratePassword();
+        private void button5_Click(object sender, EventArgs e) => 
+            textBox1.Text = GeneratePassword();
 
         private void button6_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show("Ensure you've memorized your password before continue. Continue?", "FileEncryptor", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes)
                 return;
 
-            if (listBox1.Items.Count > 0)
+            try
             {
-                foreach (var v in listBox1.Items)
+                if (listBox1.Items.Count > 0)
                 {
-                    if ($"{v}".Trim().EndsWith(".!LOCKED") || !File.Exists($"{v}"))
-                        continue;
-
-                    EncryptFile($"{v}", $"{v}.!LOCKED", textBox1.Text);
-                    File.Delete($"{v}");
-                }
-            }
-
-            if (listBox2.Items.Count > 0)
-            {
-                foreach (var v in listBox2.Items)
-                {
-                    foreach (var nameFile in Directory.GetFiles($"{v}").Where(nameFile => !(nameFile.Trim().EndsWith(".!LOCKED") || !File.Exists(nameFile))))
+                    foreach (var file in listBox1.Items)
                     {
-                        EncryptFile(nameFile, $"{nameFile}.!LOCKED", textBox1.Text);
-                        File.Delete(nameFile);
+                        if (!($"{file}".Trim().EndsWith(".!LOCKED") || !File.Exists($"{file}")))
+                        {
+                            EncryptFile($"{file}", $"{file}.!LOCKED", textBox1.Text);
+                            File.Delete($"{file}");
+                        }
                     }
                 }
-            }
 
-            MessageBox.Show("Completed operation!", "FileEncryptor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (listBox2.Items.Count > 0)
+                {
+                    foreach (var folder in listBox2.Items)
+                    {
+                        foreach (var nameFile in Directory.GetFiles($"{folder}").Where(nameFile => !nameFile.Trim().EndsWith(".!LOCKED") && File.Exists(nameFile)))
+                        {
+                            EncryptFile(nameFile, $"{nameFile}.!LOCKED", textBox1.Text);
+                            File.Delete(nameFile);
+                        }
+                    }
+                }
+
+                MessageBox.Show("Completed operation!", "FileEncryptor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show($"{error.Message}\n\n{error.StackTrace}", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void button7_Click(object sender, EventArgs e)
@@ -78,42 +103,49 @@ namespace File_Encryptor
             if (MessageBox.Show("This will return the files you encrypted before. Please double check your password, as a wrong password will result in damaged files. Continue?", "FileProtect™", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes)
                 return;
 
-            var chars = new[] { '!', '.', 'L', 'O', 'C', 'K', 'E', 'D' };
-
-            if (listBox1.Items.Count > 0)
+            try
             {
-                foreach (var v in listBox1.Items)
+                var chars = new[] { '!', '.', 'L', 'O', 'C', 'K', 'E', 'D' };
+
+                if (listBox1.Items.Count > 0)
                 {
-                    if (!(!$"{v}".Trim().EndsWith(".!LOCKED") || !File.Exists($"{v}")))
+                    foreach (var v in listBox1.Items)
                     {
-                        DecryptFile($"{v}", ($"{v}").TrimEnd(chars), textBox1.Text);
-                        File.Delete($"{v}");
+                        if (!(!$"{v}".Trim().EndsWith(".!LOCKED") || !File.Exists($"{v}")))
+                        {
+                            DecryptFile($"{v}", ($"{v}").TrimEnd(chars), textBox1.Text);
+                            File.Delete($"{v}");
+                        }
                     }
                 }
-            }
 
-            if (listBox2.Items.Count > 0)
-            {
-                foreach (var v in listBox2.Items)
+                if (listBox2.Items.Count > 0)
                 {
-                    foreach (var name_file in Directory.GetFiles($"{v}").Where(name_file => !(!name_file.Trim().EndsWith(".!LOCKED") || !File.Exists(name_file))))
+                    foreach (var v in listBox2.Items)
                     {
-                        DecryptFile(name_file, name_file.TrimEnd(chars), textBox1.Text);
-                        File.Delete(name_file);
+                        foreach (var name_file in Directory.GetFiles($"{v}").Where(name_file => name_file.Trim().EndsWith(".!LOCKED") && File.Exists(name_file)))
+                        {
+                            DecryptFile(name_file, name_file.TrimEnd(chars), textBox1.Text);
+                            File.Delete(name_file);
+                        }
                     }
                 }
-            }
 
-            MessageBox.Show("Completed operation!", "FileEncryptor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Completed operation!", "FileEncryptor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show($"{error.Message}\n\n{error.StackTrace}", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        private void EncryptFile(string szInputFile, string szOutputFile, string szPassword)
+        private void EncryptFile(string inputFile, string outputFile, string password)
         {
-            var key = new UnicodeEncoding().GetBytes(szPassword);
+            var key = new UnicodeEncoding().GetBytes(password);
 
             using (var aes = Aes.Create())
-            using (var fileStreamIn = new FileStream(szInputFile, FileMode.Open))
-            using (var fileStreamOut = new FileStream(szOutputFile, FileMode.Create))
+            using (var fileStreamIn = new FileStream(inputFile, FileMode.Open))
+            using (var fileStreamOut = new FileStream(outputFile, FileMode.Create))
             using (var cryptoStream = new CryptoStream(fileStreamOut, aes.CreateEncryptor(key, key), CryptoStreamMode.Write))
             {
                 var data = 0;
@@ -123,13 +155,13 @@ namespace File_Encryptor
             }
         }
 
-        private void DecryptFile(string szInputFile, string szOutputFile, string szPassword)
+        private void DecryptFile(string inputFile, string outputFile, string password)
         {
-            var key = new UnicodeEncoding().GetBytes(szPassword);
+            var key = new UnicodeEncoding().GetBytes(password);
 
             using (var aes = Aes.Create())
-            using (var fileStreamIn = new FileStream(szInputFile, FileMode.Open))
-            using (var fileStreamOut = new FileStream(szOutputFile, FileMode.Create))
+            using (var fileStreamIn = new FileStream(inputFile, FileMode.Open))
+            using (var fileStreamOut = new FileStream(outputFile, FileMode.Create))
             using (var cryptoStream = new CryptoStream(fileStreamIn, aes.CreateDecryptor(key, key), CryptoStreamMode.Read))
             {
                 var data = 0;
@@ -142,11 +174,9 @@ namespace File_Encryptor
         private string GeneratePassword()
         {
             var random = new Random();
-
             var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
-            var password = new string(Enumerable.Repeat(characters, 8).Select(str => str[random.Next(str.Length)]).ToArray());
 
-            return password;
+            return new string(Enumerable.Repeat(characters, 8).Select(str => str[random.Next(str.Length)]).ToArray());
         }
     }
 }
